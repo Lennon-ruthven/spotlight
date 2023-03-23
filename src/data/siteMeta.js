9803a366ffc7a537e6be593f3e6a2b764e57ba0d
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-23 01:55:50 +0000

    Add files via upload

--- src/data/siteMeta.js
+++ src/data/siteMeta.js
@@ -6,55 +6,35 @@ import logoXOR from '@/images/logos/xor.jpg'
 import logoVanilla from '@/images/logos/vanilla.svg'
 import logoGolang from '@/images/logos/icons8-golang.svg'
 import logoKubernetes from '@/images/logos/kubernetes.svg'
-import logoCaptainhook from '@/images/logos/captainhook.svg'  
+import logoCaptainhook from '@/images/logos/captainhook.svg'
+import logoBedford from '@/images/logos/Bedford.svg'   
 const siteMeta = {
-    title: "Brian Ketelsen",
-    description: "Brian Ketelsen is a developer advocate, keynote speaker, author, and open source practitioner.",
-    copyright: "Brian Ketelsen, CC-BY-SA",
+    title: "Lennon Ruthven",
+    description: "Lennon Ruthven",
+    copyright: "Lennon Ruthven",
     author: {
-        name: "Brian Ketelsen",
-        email: "me@brian.dev",
-        twitter: "https://twitter.com/bketelsen",
+        name: "Lennon Ruthven",
+        email: "lennonruthven@outlook.com",
+        twitter: "https://twitter.com/lennonruthven",
         mastodon: "https://tty0.social/@bketelsen",
-        instagram: "https://instagram.com/bketelsen",
+        instagram: "https://instagram.com/lennonruthven",
         github: "https://github.com.com/bketelsen",
-        linkedin: "https://linkedin.com/in/brianketelsen",
-        twitch: "https://twitch.tv/bketelsen",
-        youtube: "https://youtube.com/@bketelsen",
+        linkedin: "https://www.linkedin.com/in/lennon-ruthven-9238371b3/",
+        twitch: "https://twitch.tv/",
+        youtube: "https://youtube.com/",
     },
-    siteUrl: "https://brian.dev"
+    siteUrl: "lennonruthven.me"
 }
  export const resume = [
     {
-      company: 'Amazon',
-      title: 'Principal Developer Advocate',
-      logo: logoAmazon,
-      start: '2022',
+      company: 'Tresham College',
+      title: 'L2 ED in Health and Social Care: A*',
+      logo: logoBedford,
+      start: '2021',
       end: {
-        label: 'Present',
+        label: '2022',
         dateTime: new Date().getFullYear(),
       },
-    },
-    {
-      company: 'Microsoft',
-      title: 'Principal Cloud Developer Advocate',
-      logo: logoMicrosoft,
-      start: '2017',
-      end: '2022',
-    },
-    {
-      company: 'XOR Data Exchange',
-      title: 'CIO',
-      logo: logoXOR,
-      start: '2014',
-      end: '2016',
-    },
-    {
-      company: 'Clarity Services, Inc.',
-      title: 'COO',
-      logo: logoClarity,
-      start: '2008',
-      end: '2014',
     },
   ]
 export const projects = [
