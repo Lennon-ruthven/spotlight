00d0c4ee8b11829e21bfc2c1bbf4ea4bb8135bfc
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-29 19:44:18 +0100

    Add files via upload

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   MastodonIcon
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/brian-head.jpg'
+import portraitImage2 from '@/images/brian-head2.jpg'
 import siteMeta from '@/data/siteMeta'
 import { NextSeo } from 'next-seo';
 
@@ -66,7 +66,7 @@ export default function About() {
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
               <Image
-                src={portraitImage}
+                src={portraitImage2}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="object-cover aspect-square rotate-3 rounded-2xl bg-zinc-100 dark:bg-zinc-800"
