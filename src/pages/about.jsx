695e2d426634c29c2552cdba1f2b519bf9bec327
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-29 21:19:37 +0100

    Add files via upload

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -75,7 +75,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I&apos;m Lennon. I'm from England.
+              I&apos;m Lennon. I&apos;m from England.
             </h1>
             <div className="mt-6 text-lg prose space-y-7 dark:prose-invert text-zinc-600 dark:text-zinc-400">
               <p>
