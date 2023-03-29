8ce3fd2f4ba0c16804bc1aa5e0006c63be955e27
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-29 19:49:47 +0100

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -123,7 +123,6 @@ function MobileNavigation(props) {
             <nav className="mt-6">
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
-                <MobileNavItem href="/projects">Resources</MobileNavItem>
                 <MobileNavItem href="/about">About</MobileNavItem>
               </ul>
             </nav>
