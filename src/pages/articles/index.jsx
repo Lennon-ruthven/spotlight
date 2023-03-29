2717d5336bb7d9c3c47ef14755d2b1a8bbe76cdd
Lennon-ruthven <69088595+Lennon-ruthven@users.noreply.github.com>
2023-03-30 00:46:26 +0100

    Add files via upload

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,40 +1,74 @@
+import { NextSeo } from 'next-seo';
+import { Card } from '@/components/Card'
+import { SimpleLayout } from '@/components/SimpleLayout'
+import { getAllArticles } from '@/lib/getAllArticles'
+import { formatDate } from '@/lib/formatDate'
+import  siteMeta  from '@/data/siteMeta'
 
-import { useState, useEffect } from "react"
-
-import Recent 		from '../../components/sections/articles/recent'
-
-import Color 	from '../../components/utils/page.colors.util'
-
-import colors 		from '../../content/articles/_colors.json'
-import settings 	from '../../content/_settings.json'
-
-//
-export default function Articles({ mediumArticles }) {
-	return (
-		<>
-			<Color colors={colors} />
-			<Recent mediumArticles={mediumArticles}/>
-		</>
-	)
+function Article({ article }) {
+  return (
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/articles/${article.slug}`}>
+          {article.title}
+        </Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+    </article>
+  )
 }
 
-// This gets called on every request
-export async function getServerSideProps({ res }) {
+export default function ArticlesIndex({ articles }) {
+  const headline = 'Articles'
+  const intro="All of my long-form ideas on Spirituality, Philosophy, Mindset, and more, collected in chronological order."
 
-	res.setHeader(
-		'Cache-Control',
-		'public, s-maxage=600, stale-while-revalidate=59'
-	)
+  return (
+    <>
+    <NextSeo
+      title="Articles - Lennon Ruthven"
+      description={siteMeta.description}
+      canonical="http://lennonruthven.me/articles"
+      openGraph={{
+        url: 'http://lennonruthven.me/articles',
+        siteName: 'brian.dev',
+      }}
+    />
+      <SimpleLayout
+        title={headline}
+        intro={intro}
+      >
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
 
-	console.log(settings.username.medium)
-
-	const [ mediumRSS ] = await Promise.all( [
-		fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`),
-	] )
-	
-	let [ mediumArticles ] = await Promise.all( [
-		mediumRSS.json(),
-	] )
-
-	return { props: { mediumArticles } }
+export async function getStaticProps() {
+  return {
+    props: {
+      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
+    },
+  }
 }
